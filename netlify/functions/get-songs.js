325859b7ab7a77cad84e71acd8e4151ecfad4a95
nagykorte <nicolasatilamatray@gmail.com>
2025-10-31 18:11:03 -0300

    lol2

--- netlify/functions/get-songs.js
+++ netlify/functions/get-songs.js
@@ -1,30 +1,31 @@
 // filepath: netlify/functions/get-songs.js
-const { Client } = require('pg');
+// const { Client } = require('pg');
 import { neon } from '@netlify/neon';
 const sql = neon(); // automatically uses env NETLIFY_DATABASE_URL
 
 
 
 exports.handler = async function (event, context) {
-    const client = new Client({
-        connectionString: process.env.NETLIFY_DATABASE_URL, // Set this in Netlify env vars
-        ssl: { rejectUnauthorized: false }
-    });
+    // const client = new Client({
+    //     connectionString: process.env.NETLIFY_DATABASE_URL, // Set this in Netlify env vars
+    //     ssl: { rejectUnauthorized: false }
+    // });
     
-    await client.connect();
-    await ensureTable();
+    // await client.connect();
+    // await ensureTable();
     // const res = await client.query('SELECT * FROM songs');
-    const [post] = await sql`SELECT * FROM posts WHERE id = ${postId}`;
+    const [post] = await sql`SELECT * FROM songs WHERE id = 1`;
+    const [post2] = await sql`
+        CREATE TABLE IF NOT EXISTS songs (
+            id SERIAL PRIMARY KEY,
+            title VARCHAR(100) NOT NULL,
+            artist VARCHAR(100),
+            album VARCHAR(100),
+            year VARCHAR(100),
+            lines TEXT NOT NULL
+        );`
     // async function ensureTable() {
     //     await sql(`
-    //     CREATE TABLE IF NOT EXISTS songs (
-    //         id SERIAL PRIMARY KEY,
-    //         title VARCHAR(100) NOT NULL,
-    //         artist VARCHAR(100),
-    //         album VARCHAR(100),
-    //         year VARCHAR(100),
-    //         lines TEXT NOT NULL
-    //     );`)
     // }
     // await client.end();
 
