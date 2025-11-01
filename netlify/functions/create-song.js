653f9018e78bb835c1d04c031afce767e040fe59
nagykorte <nicolasatilamatray@gmail.com>
2025-11-01 18:28:27 -0300

    ??

--- netlify/functions/create-song.js
+++ netlify/functions/create-song.js
@@ -2,12 +2,21 @@ const { neon } = require('@netlify/neon');
 const sql = neon();
 
 exports.handler = async function (song) {
-    
-    const createSong = await sql`
-            INSERT INTO songs (title, artist, album, year, lines)
-            SELECT ${song.title}, ${song.artist || null}, ${song.album || null}, ${song.year || null}, ${song.lines.join('\n')}
-            RETURNING *;`;
-    console.log("Created song:", createSong);
+    try {
+
+        let _song = JSON.parse(song);
+        const createSong = await sql`
+        INSERT INTO songs (title, artist, album, year, lines)
+        SELECT ${_song.title}, ${_song.artist || null}, ${_song.album || null}, ${_song.year || null}, ${_song.lines.join('\n')}
+        RETURNING *;`;
+        console.log("Created song:", createSong);
+    } catch (error) {
+        console.error("Error creating song:", error);
+        return {
+            statusCode: 500,
+            body: JSON.stringify({ song: _song, error: error.message || "Unknown error" })
+        }
+    }
 
     return {
         statusCode: 200,
--- netlify/functions/update-song.js
+++ netlify/functions/update-song.js
@@ -4,13 +4,21 @@ const sql = neon();
 exports.handler = async function (song) {
 
     let _song = JSON.parse(song);
-    
-    const updateSong = await sql`
-            UPDATE songs
-            SET artist = ${_song.artist || null}, album = ${_song.album || null}, year = ${_song.year || null}, lines = ${lines}, title = ${_song.title}
-            WHERE id = ${_song.id} RETURNING *;
-            `;
-    console.log("Created song:", updateSong);
+    try {
+
+        const updateSong = await sql`
+        UPDATE songs
+        SET artist = ${_song.artist || null}, album = ${_song.album || null}, year = ${_song.year || null}, lines = ${lines}, title = ${_song.title}
+        WHERE id = ${_song.id} RETURNING *;
+        `;
+        console.log("Created song:", updateSong);
+    } catch (error) {
+        console.error("Error parsing song data:", error);
+        return {
+            statusCode: 400,
+            body: JSON.stringify({song: _song, msg: error.message}),
+        };
+    }
 
     return {
         statusCode: 200,
