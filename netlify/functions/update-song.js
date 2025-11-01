72308138b1b01bd7338b4339cb968605de8d55f9
nagykorte <nicolasatilamatray@gmail.com>
2025-11-01 18:17:48 -0300

    parse song

--- netlify/functions/update-song.js
+++ netlify/functions/update-song.js
@@ -2,11 +2,13 @@ const { neon } = require('@netlify/neon');
 const sql = neon();
 
 exports.handler = async function (song) {
+
+    let _song = JSON.parse(song);
     
     const updateSong = await sql`
             UPDATE songs
-            SET artist = ${song.artist || null}, album = ${song.album || null}, year = ${song.year || null}, lines = ${lines}, title = ${song.title}
-            WHERE id = ${song.id} RETURNING *;
+            SET artist = ${_song.artist || null}, album = ${_song.album || null}, year = ${_song.year || null}, lines = ${lines}, title = ${_song.title}
+            WHERE id = ${_song.id} RETURNING *;
             `;
     console.log("Created song:", updateSong);
 
